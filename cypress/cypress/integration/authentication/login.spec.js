2e93ea2db819065c71601442f89685846900abc7
Napat <napat.wongchr@gmail.com>
2022-04-17 15:13:01 +0700

    add login command

--- cypress/cypress/integration/authentication/login.spec.js
+++ cypress/cypress/integration/authentication/login.spec.js
@@ -4,27 +4,25 @@ import { buildUser } from "../../support/generate";
 
 it("user should login successfully", () => {
   const user = buildUser();
-  cy.visit("http://localhost:3000/login").viewport(1500, 660);
-  cy.request({
-    url: "http://localhost:4000/auth/register",
-    method: "POST",
-    body: user,
-  });
-  cy.get("#username").type(user.username);
-  cy.get("#password").type(user.password);
-  cy.get(".login-button").click();
-  cy.get(".board").children().should("have.length", 2);
-  cy.url().should("eq", "http://localhost:3000/");
+  cy.visit("http://localhost:3000/login")
+    .viewport(1500, 660)
+    .request({
+      url: "http://localhost:4000/auth/register",
+      method: "POST",
+      body: user,
+    })
+    .login(user.username, user.password)
+    .get(".board")
+    .children()
+    .should("have.length", 2)
+    .url()
+    .should("eq", "http://localhost:3000/");
 });
 
 it("user should not be able to login if has wrong username", () => {
-  cy.visit("http://localhost:3000/login").viewport(1500, 660);
-  cy.get("#username")
-    .type("invalid-username")
-    .get("#password")
-    .type("valid-password")
-    .get(".login-button")
-    .click()
+  cy.visit("http://localhost:3000/login")
+    .viewport(1500, 660)
+    .login("invalid-username", "valid-password")
     .url()
     .should("eq", "http://localhost:3000/login")
     .get(".error-message")
@@ -33,13 +31,11 @@ it("user should not be able to login if has wrong username", () => {
 });
 
 it("user should not be able to login if has wrong password", () => {
-  cy.visit("http://localhost:3000/login").viewport(1500, 660);
-  cy.get("#username")
-    .type("jamestechup")
-    .get("#password")
-    .type("1321421421")
-    .get(".login-button")
-    .click()
+  cy.visit("http://localhost:3000/login")
+    .viewport(1500, 660)
+    .login("jamestechup", "invalid-password")
+    .url()
+    .should("eq", "http://localhost:3000/login")
     .get(".error-message")
     .should("exist")
     .should("have.text", "Error: password not valid");
